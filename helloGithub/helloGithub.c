145b42a5966203a387dcf457c48c9086bcc71b22
KingQiangRuan <116419955+KingQiangRuan@users.noreply.github.com>
2022-11-19 10:48:40 +0800

    Add files via upload
    first project on github

---
+++ helloGithub/helloGithub.c
@@ -0,0 +1,13 @@
+/*******************************************************************
+ * Copyright(c) 2022 by qiangruan , All right reserved.
+ * Description: this is my first c/c++ project on github,good luck.
+ * date: 2022-11-19 10:34
+********************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	printf("welcome to github world! 2022-10-19 10:30\n");
+	return 0;
+}
